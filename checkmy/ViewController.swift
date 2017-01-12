16d16858f4a75cf8dd98dee302c9e6fe03308646
Uma91 <umas2091@gmail.com>
2017-01-12 16:04:58 +0530

    string
    add a string declaration

---
+++ checkmy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  checkmy
+//
+//  Created by APPLE on 1/12/17.
+//  Copyright © 2017 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let strName = String()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
